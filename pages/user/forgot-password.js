fb596a2cfe22560b7db6c8833dc835b05a64c93f
pnthu <pnthu99@gmail.com>
2021-10-03 14:55:32 +0700

    change file structure

---
+++ pages/user/forgot-password.js
@@ -0,0 +1,104 @@
+import Head from "next/head"
+import Link from "next/link"
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { Row, Col, Input } from "antd"
+import { CheckOutlined } from "@ant-design/icons"
+import Button from "../../components/common/Button"
+import { authService } from "../../services"
+import { useState } from "react"
+
+export default function ForgotPassword() {
+  const [sent, setSent] = useState(false)
+  const [email, setEmail] = useState("")
+  const router = useRouter()
+
+  const handleInputChange = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const requestResetPassword = () => {
+    authService
+      .forgotPassword(email)
+      .then((response) => {
+        setSent((current) => !current)
+        console.log(response)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Forgot Password</title>
+      </Head>
+      <Row>
+        <Col
+          className="bg-primary flex h-screen justify-center items-center"
+          span={12}
+        >
+          <Image
+            priority
+            src="/images/new-password-icon.png"
+            className=""
+            width={527}
+            height={366}
+            alt="A drawing of a girl with red hair, red shirt, black jean standing on a laptop. The laptop has security notification."
+          />
+        </Col>
+        <Col
+          className="bg-white flex h-screen justify-center items-start"
+          span={12}
+        >
+          {sent ? (
+            <div className="mt-40 w-9/12">
+              <CheckOutlined className="text-5xl text-green-500 flex items-center justify-center" />
+              <h1 className="pt-6 pb-4 text-4xl font-medium leading-12 tracking-wide">
+                Chúng tôi đã gửi email tới {email}
+              </h1>
+              <p className="pb-4 font-normal text-base leading-7 max-w-6xl mx-auto">
+                Hãy kiểm tra hộp thư rác nếu bạn không thấy email đó trong hộp
+                thư đến.
+              </p>
+              <div className="mt-6 flex items-center justify-center">
+                <Link href="/login">
+                  <a>← Quay lại đăng nhập</a>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div className="mt-40 w-96">
+              <h1 className="pt-1 pb-4 text-4xl font-medium tracking-wide">
+                Bạn quên mật khẩu?
+              </h1>
+              <p className="font-normal text-base leading-7 max-w-6xl mx-auto">
+                Chúng tôi sẽ gửi email kèm theo hướng dẫn đặt lại mật khẩu cho
+                bạn.{" "}
+              </p>
+              <Input
+                className="mt-6 h-12 border border-primary hover:border-purple-700 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                placeholder="Nhập email của bạn"
+                type="email"
+                name="email"
+                onChange={handleInputChange}
+                value={email}
+              />
+              <div className="mt-6 flex items-center justify-center">
+                <Button onClick={requestResetPassword}>
+                  Gửi email cho tôi
+                </Button>
+              </div>
+              <div className="mt-4 flex items-center justify-center">
+                <Link href="/login">
+                  <a>← Quay lại đăng nhập</a>
+                </Link>
+              </div>
+            </div>
+          )}
+        </Col>
+      </Row>
+    </>
+  )
+}
---
+++ pages/user/password-recovered.js
@@ -0,0 +1,109 @@
+import Head from "next/head"
+import Link from "next/link"
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { Row, Col, Input } from "antd"
+import { CheckOutlined } from "@ant-design/icons"
+import Button from "../../components/common/Button"
+import { authService } from "../../services"
+import { useState } from "react"
+
+export default function PasswordRecovery() {
+  const [password, setPassword] = useState(null)
+  const [recovered, setRecovered] = useState(false)
+  const router = useRouter()
+
+  const handleInputChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const requestNewPassword = () => {
+    // TODO: Contact with BE to proceed next step
+    // authService.passwordChange(password)
+    // .then(response => {
+    //   setRecovered(current => !current);
+    //   console.log(response);
+    // })
+    // .catch(e => {
+    //   console.log(e);
+    // });
+    setRecovered((current) => !current)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Password Recovery</title>
+      </Head>
+      {recovered ? (
+        <Row>
+          <Col
+            className="bg-secondary flex h-screen justify-center items-center"
+            span={12}
+          >
+            <Image
+              priority
+              src="/images/password-recovery-icon.png"
+              className=""
+              width={527}
+              height={366}
+              alt="A drawing of a girl with red hair, red shirt, black jean standing on a laptop. The laptop has security notification."
+            />
+          </Col>
+          <Col
+            className="bg-white flex h-screen justify-center items-start"
+            span={12}
+          >
+            <div className="mt-40">
+              <CheckOutlined className="text-5xl text-green-500 flex items-center justify-center" />
+              <h1 className="pt-6 pb-4 text-4xl text-center font-medium leading-12 tracking-wide">
+                Mật khẩu của bạn đã được khôi phục!
+              </h1>
+              <div className="mt-6 flex items-center justify-center">
+                <Button>Đăng Nhập</Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+          <Col
+            className="bg-primary flex h-screen justify-center items-center"
+            span={12}
+          >
+            <Image
+              priority
+              src="/images/new-password-icon.png"
+              className=""
+              width={527}
+              height={366}
+              alt="A drawing of a girl with red hair, blue shirt, black jean standing on a laptop. The laptop has security notification."
+            />
+          </Col>
+          <Col
+            className="bg-white flex h-screen justify-center items-start"
+            span={12}
+          >
+            <div className="mt-40 w-96">
+              <h1 className="pt-1 pb-4 text-4xl font-medium tracking-wide">
+                Khôi phục mật khẩu
+              </h1>
+              <p className="font-normal text-base leading-7 max-w-6xl mx-auto">
+                Hãy nhập mật khẩu mới cho tài khoản của bạn
+              </p>
+              <Input.Password
+                className="mt-6 h-12 border border-primary hover:border-purple-700 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                placeholder="Nhập mật khẩu mới"
+                onChange={handleInputChange}
+                value={password}
+              />
+              <div className="mt-6 flex items-center justify-center">
+                <Button onClick={requestNewPassword}>Khôi phục mật khẩu</Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      )}
+    </>
+  )
+}
