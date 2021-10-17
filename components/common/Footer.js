545807168a26faa79a5ce046a90918f5bf1e3331
ThaoNP <npthao1312@gmail.com>
2021-10-17 19:35:43 +0700

    Make Mobile Footer

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-  <div class="container flex mx-auto py-6 justify-center" > 
-    <span class="mx-3 ">@2021 SHECODES VIETNAM</span>
-    <span class="mx-3"> | </span>
-    <a class="mx-3">Contact</a>
-    <a class="mx-3">Terms of use</a>
-    <a class="mx-3">Donate</a>
-    <a class="mx-3">FAQ</a>
-  </div>
+    <div className="">
+      <div className="container flex mx-auto py-6 justify-center items-center md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+        <span className="mx-3">@2021 SHECODES VIETNAM</span>
+        <span className="hidden md:block mx-3"> | </span>
+        <a className="mt-4 md:mt-0 mx-3">Contact</a>
+        <a className="mt-2 md:mt-0 mx-3">Terms of use</a>
+        <a className="mt-2 md:mt-0 mx-3">Donate</a>
+        <a className="mt-2 md:mt-0 mx-3">FAQ</a>
+      </div>
+    </div>
   );
 };
 
--- components/common/Header.js
+++ components/common/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "../../public/images/svglogoWithoutBg.svg";
 import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="ml-20 py-5 mx-auto">
-      <Image src={logo} alt="logo" width={130} height={33} />
+    <div className="ml-20 py-4 mx-auto">
+      <Image src="/images/logo.png" alt="logo" width={130} height={33} />
     </div>
   );
 };
