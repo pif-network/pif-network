c6c63897939d95203226d297c0b879e8f4d7f337
thunpham <thunpham@kms-technology.com>
2021-12-05 00:07:12 +0700

    update complete profile

--- features/user/create/CompleteProfile.js
+++ features/user/create/CompleteProfile.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as Yup from 'yup'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useFormik } from 'formik'
+import { FieldArray, FormikProvider, useFormik } from 'formik'
 import { ArrowLeftOutlined } from '@ant-design/icons'
 import { Button } from '../../../components/button'
 import UserService from '../../../services/UserService'
@@ -20,12 +20,18 @@ const CompleteProfile = () => {
       .min(10, 'Số điện thoại bao gồm 10 ký số')
       .max(10, 'Số điện thoại bao gồm 10 ký số')
       .required('Vui lòng nhập số điện thoại'),
-    school: Yup.string()
-      .max(50, 'Công ty/Trường học không được dài quá 50 ký tự')
-      .required('Vui lòng nhập tên trường học/công ty'),
-    exp: Yup.string()
-      .max(50, 'Công việc/ngành học không được dài quá 50 ký tự')
-      .required('Vui lòng nhập công việc/ngành học'),
+    school: Yup.array().of(
+      Yup.object().shape({
+        name: Yup.string().max(50, 'Tên trường học không được dài quá 50 ký tự'),
+        major: Yup.string().max(50, 'Tên ngành học không được dài quá 50 ký tự'),
+      }),
+    ),
+    exp: Yup.array().of(
+      Yup.object().shape({
+        name: Yup.string().max(50, 'Tên công ty không được dài quá 50 ký tự'),
+        position: Yup.string().max(50, 'Tên công việc không được dài quá 50 ký tự'),
+      }),
+    ),
     agreed: Yup.boolean().test('checked', 'Vui lòng chọn "Đồng ý" khi hoàn tất đăng ký', (value, context) => value),
   })
 
@@ -33,8 +39,18 @@ const CompleteProfile = () => {
     initialValues: {
       date_of_birth: '',
       phone: '',
-      school: '',
-      exp: '',
+      school: [
+        {
+          name: '',
+          major: '',
+        },
+      ],
+      exp: [
+        {
+          name: '',
+          position: '',
+        },
+      ],
       agreed: false,
     },
     validationSchema,
@@ -69,55 +85,103 @@ const CompleteProfile = () => {
               Hoàn thành profile để mentor hiểu rõ hơn về bạn!
             </h1>
             <div>
-              <form onSubmit={formik.handleSubmit}>
-                {message && <div className="mt-4 text-red-500 flex items-center justify-center">{message}</div>}
-                <div style={{ display: 'grid', rowGap: '2.25rem' }}>
-                  <Input
-                    name="date_of_birth"
-                    type="date"
-                    placeholder="Ngày/tháng/năm sinh"
-                    onChange={formik.handleChange}
-                    value={formik.values.date_of_birth}
-                  />
-                  <Input
-                    name="phone"
-                    type="tel"
-                    placeholder="Số điện thoại"
-                    onChange={formik.handleChange}
-                    value={formik.values.phone}
-                  />
-                  <Input
-                    name="school"
-                    type="text"
-                    placeholder="Công ty/trường học"
-                    onChange={formik.handleChange}
-                    value={formik.values.school}
-                  />
-                  <Input
-                    name="exp"
-                    type="text"
-                    placeholder="Công việc/ngành học"
-                    onChange={formik.handleChange}
-                    value={formik.values.exp}
-                  />
-                </div>
-                <div className="flex justify-start items-center" style={{ width: '80%', margin: '1.25rem 0' }}>
-                  <Input name="agreed" type="checkbox" onChange={formik.handleChange} value={formik.values.agreed} />
-                  <p style={{ marginLeft: '0.5rem' }}>
-                    Tôi đã đọc và đồng ý với các &nbsp;
-                    <Link href="#">điều khoản dịch vụ và chính sách bảo mật</Link>
-                  </p>
-                </div>
-                <Button variant="contained" type="submit">
-                  {loading ? (
-                    <div className=" flex justify-center items-center">
-                      <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
-                    </div>
-                  ) : (
-                    <>Hoàn thành</>
-                  )}
-                </Button>
-              </form>
+              <FormikProvider value={formik}>
+                <form onSubmit={formik.handleSubmit}>
+                  {message && <div className="mt-4 text-red-500 flex items-center justify-center">{message}</div>}
+                  <div style={{ display: 'grid', rowGap: '2.25rem' }}>
+                    <Input
+                      name="date_of_birth"
+                      type="date"
+                      placeholder="Ngày/tháng/năm sinh"
+                      onChange={formik.handleChange}
+                      value={formik.values.date_of_birth}
+                    />
+                    <Input
+                      name="phone"
+                      type="tel"
+                      placeholder="Số điện thoại"
+                      onChange={formik.handleChange}
+                      value={formik.values.phone}
+                    />
+                    <FieldArray
+                      name="school"
+                      render={arrayHelpers => (
+                        <div>
+                          {formik.values.school.map((value, index) => (
+                            <div key={index} style={{ marginBottom: '1rem' }}>
+                              <Input
+                                name={`school[${index}].name`}
+                                value={formik.values.school[index].name}
+                                onChange={formik.handleChange}
+                                style={{ marginBottom: '0.75rem' }}
+                                placeholder="Trường học"
+                              />
+                              <Input
+                                name={`school[${index}].major`}
+                                value={formik.values.school[index].major}
+                                onChange={formik.handleChange}
+                                placeholder="Ngành học"
+                              />
+                              <Button variant="text" onClick={() => arrayHelpers.remove(index)} type="button">
+                                X
+                              </Button>
+                            </div>
+                          ))}
+                          <Button variant="contained" onClick={() => arrayHelpers.push({ name: '', major: '' })}>
+                            Add school
+                          </Button>
+                        </div>
+                      )}
+                    />
+                    <FieldArray
+                      name="exp"
+                      render={arrayHelpers => (
+                        <div>
+                          {formik.values.exp.map((value, index) => (
+                            <div key={index} style={{ marginBottom: '1rem' }}>
+                              <Input
+                                name={`exp[${index}].name`}
+                                value={formik.values.exp[index].name}
+                                onChange={formik.handleChange}
+                                style={{ marginBottom: '0.75rem' }}
+                                placeholder="Công ty"
+                              />
+                              <Input
+                                name={`exp[${index}].position`}
+                                value={formik.values.exp[index].position}
+                                onChange={formik.handleChange}
+                                placeholder="Công việc"
+                              />
+                              <Button variant="text" onClick={() => arrayHelpers.remove(index)} type="button">
+                                X
+                              </Button>
+                            </div>
+                          ))}
+                          <Button variant="contained" onClick={() => arrayHelpers.push({ name: '', position: '' })}>
+                            Add experience
+                          </Button>
+                        </div>
+                      )}
+                    />
+                  </div>
+                  <div className="flex justify-start items-center" style={{ width: '80%', margin: '1.25rem 0' }}>
+                    <Input name="agreed" type="checkbox" onChange={formik.handleChange} value={formik.values.agreed} />
+                    <p style={{ marginLeft: '0.5rem' }}>
+                      Tôi đã đọc và đồng ý với các &nbsp;
+                      <Link href="#">điều khoản dịch vụ và chính sách bảo mật</Link>
+                    </p>
+                  </div>
+                  <Button variant="contained" type="submit">
+                    {loading ? (
+                      <div className=" flex justify-center items-center">
+                        <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
+                      </div>
+                    ) : (
+                      <>Hoàn thành</>
+                    )}
+                  </Button>
+                </form>
+              </FormikProvider>
             </div>
             <div className="flex items-center" style={{ marginTop: '0.75rem' }}>
               <ArrowLeftOutlined style={{ marginRight: '6px' }} />
