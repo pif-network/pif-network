c00121cb54e5cc0b6f8c06fa5132bd3ea20a137a
Kimthanh11 <18047@student.vgu.edu.vn>
2022-06-17 15:41:04 +0700

    [Update] Custom Button

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -1,141 +1,42 @@
 import styled, { css } from 'styled-components'
-import { string } from 'yup';
-import { FlagLine } from '../svgs/Icons';
-
-// interface Props {
-//   variant: string
-// }
-
-// const Style = styled.button`
-//   display: flex;
-//   flex-grow: 0;
-//   flex: none;
-//   align-items: center;
-//   text-align: center;
-
-//   font-family: Be Vietnam;
-//   font-style: normal;
-//   font-weight: bold;
-//   font-size: 14px;
-//   font-feature-settings: 'case' on;
-
-//   line-height: 16px;
-
-//   padding: 6px 6px 6px 8px;
-//   margin: 0px 10px;
-
-//   order: 0;
-
-//   ${({ variant }: Props) =>
-//     variant == 'outline' &&
-//     css`
-//       background: #ffffff;
-//       color: #6f3e97;
-//       border: 1px solid #6f3e97;
-//       box-sizing: border-box;
-//       border-radius: 4px;
-
-//       &:hover {
-//         background: #f3eff8;
-//         color: #6f3e97;
-//       }
-
-//       &:focus {
-//         background: #e7dbf8;
-//         color: #6f3e97;
-//       }
-
-//       &:active {
-//         color: #6f3e97;
-//         background: #e7dbf8;
-//         background-size: 100%;
-//       }
-
-//       &:disabled {
-//         cursor: default;
-//         color: grey;
-//       }
-//     `}
-
-//   ${({ variant }: Props) =>
-//     variant == 'contained' &&
-//     css`
-//       background: #6f3e97;
-//       color: white;
-//       border: 1px solid #6f3e97;
-//       box-sizing: border-box;
-//       border-radius: 4px;
-
-//       &:hover {
-//         background: #f3eff8;
-//         color: #6f3e97;
-//       }
-
-//       &:focus {
-//         background: #e7dbf8;
-//       }
-
-//       &:active {
-//         background: #e7dbf8;
-//         background-size: 100%;
-//       }
-
-//       &:disabled {
-//         cursor: default;
-//         color: grey;
-//       }
-//     `}
-
-
-//   ${({ variant }: Props) =>
-//     variant == 'text' &&
-//     css`
-//       background: #ffffff;
-//       color: #6f3e97;
-//       border: 1px #6f3e97;
-//       box-sizing: border-box;
-//       border-radius: 4px;
-
-//       &:hover {
-//         background: #f3eff8;
-//         color: #6f3e97;
-//       }
-
-//       &:focus {
-//         background: #e7dbf8;
-//       }
-
-//       &:active {
-//         background: #d8bfd8;
-//         background-size: 100%;
-//       }
-
-//       &:disabled {
-//         cursor: default;
-//         color: grey;
-//       }
-//     `}
-// `
+import { string } from 'yup'
+import Link from 'next/link'
 
 interface Props {
-  children: JSX.Element[] | JSX.Element;
-  type: string; //bg color: outlined, filled
-  variant: string; //text color: neutral, primary
-  small: boolean; //size of button
+  content: string
+  type: string //bg color: outlined, filled
+  variant: string //text color: black, white, primary
+  size: string //size of button
+  fontSize: string
+  onClick: any
+  link: string
 }
 
-const Button = ({ children, small, variant, type}: Props) => {
-  var size = "";
-  var bgcolor = "";
-  var textcolor = "";
-  small ? size = "py-1 px-6" : size = "py-2 px-20 h-12 w-60";
-  type == "filled" ? bgcolor = "primary-800" : (type == "outlined" ? bgcolor = "gray-50" : "");
-  variant == "neutral" ? (type == "filled" ? textcolor = "white" : textcolor = "gray-700") : textcolor = "primary-400";
+const Button = ({
+  content,
+  size,
+  variant,
+  type,
+  fontSize,
+  onClick,
+  link,
+}: Props) => {
+  let sizeButton = ''
+  let bgcolor = ''
+  size == 'small' ? (sizeButton = 'py-1 px-6') : ''
+  size == 'medium' ? (sizeButton = 'py-2 px-20 h-12 w-60') : ''
+  type == 'filled' ? (bgcolor = 'primary-800') : ''
+  type == 'outlined' ? (bgcolor = 'gray-50') : ''
   return (
     <div>
-      <button className={`bg-${bgcolor} text-${textcolor} font-bold ${size} rounded-xl `}>
-        {children}
-      </button>
+      <Link href={link}>
+        <button
+          className={`bg-${bgcolor} text-${variant} font-bold ${sizeButton} rounded-xl text-${fontSize}`}
+          onClick={onClick}
+        >
+          {content}
+        </button>
+      </Link>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,14 @@ import Head from 'next/head'
 import { UserService } from '~/services'
 import { Mentor } from '~/lib/types/user'
 import { getErrorMessage } from '~/lib/types/service'
-import { Skeleton, Hero, Benefits, Values, Feedback, Button } from '~/components/ui'
+import {
+  Skeleton,
+  Hero,
+  Benefits,
+  Values,
+  Feedback,
+  Button,
+} from '~/components/ui'
 import { MentorCard } from '~/components/mentor'
 import { FlagLine } from '~/components/ui/svgs/Icons'
 
@@ -36,64 +43,6 @@ const HomePage = () => {
         <Benefits />
         <Values />
 
-        {/* <Button small={true} variant="neutral" type="filled">
-          <span>Button</span>
-        </Button>
-
-        <Button small={true} variant="neutral" type="outlined">
-          <span>Button</span>
-        </Button>
-
-        <Button small={true} variant="neutral" type="filled">
-          <div className='inline-flex'>
-            <div>
-              Button 
-            </div>
-            <div>
-              <FlagLine colour='white'></FlagLine>
-            </div>
-          </div>
-        </Button>
-        
-        <Button small={true} variant="neutral" type="outlined">
-          <div className='inline-flex'>
-            <div>
-              Button 
-            </div>
-            <div>
-              <FlagLine colour='black'></FlagLine>
-            </div>
-          </div>
-        </Button>
-
-        <Button small={false} variant="neutral" type="filled">
-          <span>Button</span>
-        </Button>
-
-        <Button small={false} variant="neutral" type="filled">
-          <span>Button &#10095;</span>
-        </Button>
-
-        <Button small={false} variant="neutral" type="outlined">
-          <span>Button</span>
-        </Button>
-
-        <Button small={false} variant="neutral" type="outlined">
-          <div className='inline-flex'>
-            <div>
-              Button 
-            </div>
-            <div>
-              <FlagLine colour='black'></FlagLine>
-            </div>
-          </div>
-        </Button>
-
-        <Button small={false} variant="primary" type="outlined">
-          <span>Button</span>
-        </Button> */}
-  
-        
         {/* <div className="p-8 lg:p-24">
           <div className="flex flex-wrap">
             {mentors ? (
