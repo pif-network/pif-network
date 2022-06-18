76e14cebb5a6cea40ab04a471415ec7a60e6809d
TienLePham <tienlepham094@gmail.com>
2022-06-18 21:27:55 +0700

    [Update] Tailwind config and img folder

--- components/ui/Values/ValueCard.tsx
+++ components/ui/Values/ValueCard.tsx
@@ -3,20 +3,22 @@ export interface Card {
   content: string
   url: string
 }
-function ValueCard({ Card }: { Card: Partial<Card> }) {
+function ValueCard({ card }: { card: Partial<Card> }) {
   return (
     <div
       className="rounded-xl relative md:mt-4 overflow-hidden md:h-[298px] md:w-[525px] "
       style={{
-        background: `url(${Card.url}) no-repeat`,
+        background: `url(${card.url}) no-repeat`,
         backgroundSize: 'contain',
       }}
     >
-      <div className="bg-gradient-to-b from-[#170330] absolute top-0 right-0 left-0 bottom-1/2"></div>
+      <div className="absolute top-0 right-0 left-0 bottom-0 card-mask"></div>
       <div className="absolute left-[36px] top-[29px] font-lora">
-        <h4 className="font-medium text-white text-heading pb-1">{Card.title}</h4>
+        <h5 className="font-medium text-white text-heading pb-1 word-[-4px]">
+          {card.title}
+        </h5>
         <p className="text-white font-manrope font-regular text-sub-heading">
-          {Card.content}
+          {card.content}
         </p>
       </div>
     </div>
--- components/ui/Values/index.tsx
+++ components/ui/Values/index.tsx
@@ -1,45 +1,48 @@
 import ValueCard from './ValueCard'
 import { Card } from './ValueCard'
+import SectionTitle from '../SectionTitle'
+import valueImg1 from '~/assets/value-card/value-card-1.png'
+import valueImg2 from '~/assets/value-card/value-card-2.png'
+import valueImg3 from '~/assets/value-card/value-card-3.png'
+import valueImg4 from '~/assets/value-card/value-card-4.png'
 
 const VALUES = [
   {
     title: 'Mở rộng kết nối',
     content:
       'Kết nối 1-1 với nhiều mentor có kinh nghiệm  và background phù hợp với nhu cầu của bạn',
-    url: '/images/value-card-1.png',
+    url: `${valueImg1.src}`,
   },
   {
     title: 'Tư vấn lộ trình',
     content:
       'Nhận được tư vấn về lộ trình và cách thức phát triển cơ hội nghề nghiệp trong lĩnh vực công nghệ',
-    url: '/images/value-card-2.png',
+    url:  `${valueImg2.src}`,
   },
   {
     title: 'Review CV/ Resume',
     content:
       'Được hỗ trợ review CV/ Resume cũng như phỏng vấn thử bởi những mentor dày dặn kinh nghiệm',
-    url: '/images/value-card-3.png',
+    url: `${valueImg3.src}`,
   },
   {
     title: 'Chia sẻ kiến thức',
     content:
       'Cùng nhau chia sẻ về phương pháp tự học, rèn luyện kỹ năng và kiến thức trong lĩnh vực mong muốn',
-    url: '/images/value-card-4.png',
+    url:  `${valueImg4.src}`,
   },
 ] as Card[]
 
-const ValueSection = () => {
+const Value = () => {
   return (
     <div className="lg:px-[170px] m-auto max-w-[1440px]">
-      <h3 className="font-lora text-title-sm font-semi-bold w-full">
-        Cách chúng tôi hỗ trợ bạn
-      </h3>
+      <SectionTitle content='Cách chúng tôi hỗ trợ bạn'/>
       <div className="flex flex-wrap items-center justify-center gap-4">
         {VALUES.map((item, index) => (
-          <ValueCard key={index} Card={item} />
+          <ValueCard key={index} card={item} />
         ))}
       </div>
     </div>
   )
 }
-export default ValueSection
+export default Value
