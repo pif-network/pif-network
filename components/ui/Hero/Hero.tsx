76271b3fe730722b48a918c379081baf02bd0bd0
Tuong Vy <tuongvy27082002@gmail.com>
2022-06-22 22:49:53 +0700

    update Hero section

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -16,31 +16,31 @@ import Button from '../../../components/ui/Button'
 import { RightOutlined } from '@ant-design/icons'
 const Hero = () => {
   return (
-    <div className="flex md:flex-row flex-col py-32 px-24 bg-[#F2F2F2]">
+    <div className="flex md:flex-row flex-col py-32 px-24 bg-white">
       <div className="flex flex-col basis-full md:basis-1/2 order-2 md:order-1">
-        <div className="font-lora font-semi-bold word-[-0.2rem] lg:word-[-1rem] text-heading md:text-title-sm lg:text-title">
+        <div className="font-lora font-semi-bold word-[-0.3rem] text-title-sm lg:word-[-1rem]  md:text-title-sm lg:text-title">
           <div className="flex flex-col items-center md:items-start">
             <div>Sự giúp đỡ</div>
             <div>mà bạn</div>
             <div className="flex flex-row ">
               đang{' '}
-              <span className="flex flex-row items-center ml-3 font-[500] border-2 border-dashed border-black rounded">
-                <div className="relative font-lora font-medium italic mx-1 before:absolute before:bg-[#120227] before:h-1 before:left-0 before:right-0 before:bottom-2">
+              <span className="flex flex-row items-center ml-3 font-medium border-2 border-dashed border-black rounded">
+                <div className="relative font-lora font-medium italic mx-1 before:absolute before:bg-black before:h-1 before:left-0 before:right-0 sm:before:bottom-1 md:before:bottom-2">
                   tìm kiếm
                 </div>
-                <SearchIcon className="ml-2 w-12 h-12" />
+                <SearchIcon className="ml-1 w-8 h-8 md:ml-2 md:w-12 md:h-12" />
               </span>
             </div>
           </div>
         </div>
-        <div className="font-manrope leading-7 text-[21px] mt-16 text-[#120227]">
-          Trò chuyện, chia sẻ, nâng cao kĩ năng chuyên
-          <br />
-          môn, mở rộng hiểu biết và khám phá những
-          <br />
-          insights mới lạ từ các mentor dày dặn kinh nghiệm
-          <br />
-          của chúng tôi.
+        <div className="flex flex-col font-manrope leading-7 text-[14px] mt-4 text-primary-900 items-center md:mt-16 md:items-start md:text-[21px] ">
+          <div>Trò chuyện, chia sẻ, nâng cao kĩ năng chuyên</div>
+          {/* <br /> */}
+          <div>môn, mở rộng hiểu biết và khám phá những</div>
+          {/* <br /> */}
+          <div>insights mới lạ từ các mentor dày dặn kinh nghiệm</div>
+          {/* <br /> */}
+          <div>của chúng tôi.</div>
         </div>
 
         <div className="flex flex-row mt-8 gap-3">
@@ -52,7 +52,7 @@ const Hero = () => {
           </Button>
           <Button
             variant="outline"
-            className="text-body  px-8 py-2 pt-2 font-manrope font-[600]"
+            className="text-body  px-8 py-2 pt-2 font-manrope font-semi-bold"
           >
             <div className="flex gap-2 items-center">
               <div>Về SheCodes</div>
