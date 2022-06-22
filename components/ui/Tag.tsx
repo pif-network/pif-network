f8f5e86af417703afd727eaba727a800cf568ccb
Tam Nguyen <tamnguyenle.dev@gmail.com>
2022-06-22 17:53:26 +0700

    Add stylings, rename stuffs

--- components/ui/Tag.tsx
+++ components/ui/Tag.tsx
@@ -1,27 +1,27 @@
 import { CloseOutlined } from '@ant-design/icons'
 
-type TagType = 'dark' | 'light'
+type TagType = 'filled' | 'outlined'
 type TagColorPreset = 'gray' | 'primary' | 'red' | 'cyan'
 
 interface TagProps {
   color?: TagColorPreset
   type?: TagType
-  closable?: boolean
-  onClose?: (e: React.MouseEvent<HTMLElement>) => void
+  deletable?: boolean
+  onDelete?: (e: React.MouseEvent<HTMLElement>) => void
 }
 
 const hoverPresetByType = {
   primary: {
-    light: 'hover:text-primary-400 hover:border-primary-400',
-    dark: 'hover:border-primary-400 hover:bg-primary-300',
+    outlined: 'hover:text-primary-400 hover:border-primary-400',
+    filled: 'hover:border-primary-400 hover:bg-primary-300',
   },
   red: {
-    light: 'hover:text-red-300 hover:border-red-300',
-    dark: 'hover:border-red-300 hover:bg-red-200',
+    outlined: 'hover:text-red-300 hover:border-red-300',
+    filled: 'hover:border-red-300 hover:bg-red-200',
   },
   cyan: {
-    light: 'hover:text-cyan-300 hover:border-cyan-300',
-    dark: 'hover:border-cyan-300 hover:bg-cyan-200',
+    outlined: 'hover:text-cyan-300 hover:border-cyan-300',
+    filled: 'hover:border-cyan-300 hover:bg-cyan-200',
   },
   gray: undefined,
 }
@@ -34,30 +34,29 @@ const deleteBgByType = {
 }
 
 const defaultPresetByType = {
-  light: 'text-gray-600 bg-white border-gray-400',
-  dark: 'text-gray-50 bg-gray-400 border-gray-700',
+  outlined: 'text-gray-600 bg-white border-gray-400',
+  filled: 'text-gray-50 bg-gray-400 border-gray-700',
 }
 
 export const Tag: React.FC<TagProps> = ({
   children,
   color = 'gray',
-  type = 'light',
-  closable,
-  onClose,
+  type = 'outlined',
+  deletable,
+  onDelete,
 }) => {
   const colorClasses = `${defaultPresetByType[type]} ${hoverPresetByType[color]?.[type]}`
   return (
     <div
-      className={`${colorClasses} border rounded-3xl px-2 py-1 inline-block relative group`}
+      className={`${colorClasses} border font-semi-bold font-manrope text-caption rounded-3xl px-2 py-1 inline-block relative group`}
     >
       {children}
-      {closable && (
+      {deletable && (
         <CloseOutlined
-          className={`invisible group-hover:visible text-body-xs absolute top-1 right-2 h-[22px] w-3.5 leading-5 cursor-pointer ${
-            type === 'light' ? 'bg-white' : deleteBgByType[color]
+          className={`invisible group-hover:visible text-caption absolute top-[0.2rem] h-[1.125rem] right-1 w-3.5 cursor-pointer ${
+            type === 'outlined' ? 'bg-white' : deleteBgByType[color]
           }`}
-          style={{ fontSize: '12px' }}
-          onClick={onClose}
+          onClick={onDelete}
         />
       )}
     </div>
