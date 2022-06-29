6664067dde7afc35f78bffc227743926ca099037
imsumoregn <an12312123@gmail.com>
2022-06-29 06:32:56 +0100

    Update Link with external href and newtab mechanism

--- components/ui/Link/index.tsx
+++ components/ui/Link/index.tsx
@@ -1,20 +1,68 @@
 import { forwardRef, PropsWithChildren } from 'react'
 import NextLink, { LinkProps } from 'next/link'
 
-const Link = forwardRef<HTMLAnchorElement, PropsWithChildren<LinkProps>>(
-  ({ href, prefetch, replace, scroll, shallow, locale, children }, ref) => (
-    <NextLink
-      href={href}
-      replace={replace}
-      scroll={scroll}
-      shallow={shallow}
-      locale={locale}
-      prefetch={prefetch}
-      passHref
-    >
-      <a className="text-primary underline hover:text-violet">{children}</a>
-    </NextLink>
-  ),
+import type { ExternalHrefProps } from '~/lib/types'
+
+type Props = {
+  className?: string
+  newTab?: boolean
+} & ExternalHrefProps &
+  LinkProps
+
+const Link = forwardRef<HTMLAnchorElement, PropsWithChildren<Props>>(
+  (
+    {
+      className,
+      newTab,
+      external,
+      href,
+      prefetch,
+      replace,
+      scroll,
+      shallow,
+      locale,
+      children,
+      ...others
+    },
+    ref,
+  ) => {
+    if (external) {
+      return (
+        <a
+          className={className}
+          href={href as string}
+          ref={ref}
+          rel={newTab ? 'noopener noreferrer' : undefined}
+          target={newTab ? '_blank' : undefined}
+          {...others}
+        >
+          {children}
+        </a>
+      )
+    }
+
+    return (
+      <NextLink
+        href={href}
+        replace={replace}
+        scroll={scroll}
+        shallow={shallow}
+        locale={locale}
+        prefetch={prefetch}
+        passHref
+      >
+        <a
+          className={className}
+          ref={ref}
+          rel={newTab ? 'noopener noreferrer' : undefined}
+          target={newTab ? '_blank' : undefined}
+          {...others}
+        >
+          {children}
+        </a>
+      </NextLink>
+    )
+  },
 )
 
 export default Link
