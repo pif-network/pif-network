4f45da84fceca9000ed1e7c09a5c392d9f04c05b
imsumoregn <an12312123@gmail.com>
2022-07-02 02:35:58 +0100

    Adjust styling: align and center cards

--- components/ui/Values/index.tsx
+++ components/ui/Values/index.tsx
@@ -1,6 +1,8 @@
-import ValueCards from './ValueCards'
-import SectionTitle from '../SectionTitle'
+import ValueCard from './ValueCard'
 import type { Card } from './ValueCard'
+
+import { SectionTitle } from '~/components/ui'
+
 import valueImg1 from '~/assets/value-card/value-card-1.png'
 import valueImg2 from '~/assets/value-card/value-card-2.png'
 import valueImg3 from '~/assets/value-card/value-card-3.png'
@@ -47,13 +49,88 @@ const VALUES = [
 
 const Values = () => {
   return (
-    <div className="2xl:px-[170px] m-auto max-w-[1440px] md:px-[50px]">
-      <SectionTitle
-        content="Cách chúng tôi hỗ trợ bạn"
-        className="hidden md:block -ml-[4px]"
-      />
-      <ValueCards card={VALUES} />
-    </div>
+    <>
+      <section className="hidden md:inline">
+        <div className="flex flex-col max-w-[525px] xl:grid xl:grid-cols-[525px_525px] xl:max-w-6xl xl:gap-x-2 m-auto justify-center">
+          <SectionTitle
+            content="Cách chúng tôi hỗ trợ bạn"
+            className="col-span-2"
+          />
+          {VALUES.map(item => (
+            <ValueCard key={item.id} card={item} />
+          ))}
+        </div>
+      </section>
+
+      <section className="inline md:hidden">
+        <div className="relative w-full h-full md:hidden">
+          <div
+            className="w-full h-[880px] relative left-0 right-0"
+            style={{
+              background: `url(${VALUES[1]?.urlMobile}) no-repeat`,
+              backgroundSize: 'cover',
+            }}
+          >
+            <div className="absolute font-lora bottom-[70px] left-[30px] w-[197px]">
+              <h5 className="font-semi-bold text-white text-heading pb-1 word-[-8px]">
+                {VALUES[1]?.title}
+              </h5>
+              <p className="text-white font-manrope font-regular text-body-sm">
+                {VALUES[1]?.content}
+              </p>
+            </div>
+          </div>
+          <div
+            className="w-full relative -mt-[180px] h-[880px] -z-10"
+            style={{
+              background: `url(${VALUES[0]?.urlMobile}) no-repeat`,
+              backgroundSize: 'cover',
+            }}
+          >
+            <div className="absolute font-lora top-[200px] right-[30px] w-[212px]">
+              <h5 className="font-semi-bold text-primary-900 text-heading pb-1 word-[-8px]">
+                {VALUES[0]?.title}
+              </h5>
+              <p className="text-primary-900 font-manrope font-regular text-body-sm text-right">
+                {VALUES[0]?.content}
+              </p>
+            </div>
+          </div>
+          <div
+            className="w-full -mt-[80px] right-0 h-[880px] -z-20 relative"
+            style={{
+              background: `url(${VALUES[2]?.urlMobile}) no-repeat`,
+              backgroundSize: 'cover',
+            }}
+          >
+            <div className="absolute font-lora bottom-[90px] right-[30px] w-[212px]">
+              <h5 className="text-right font-semi-bold text-primary-900 text-heading pb-1 word-[-8px]">
+                Đánh giá CV
+              </h5>
+              <p className="text-primary-900 font-manrope font-regular text-body-sm text-right">
+                {VALUES[2]?.content}
+              </p>
+            </div>
+          </div>
+          <div
+            className="w-full relative -mt-[80px] right-0 h-[880px] -z-30"
+            style={{
+              background: `url(${VALUES[3]?.urlMobile}) no-repeat`,
+              backgroundSize: 'cover',
+            }}
+          >
+            <div className="absolute font-lora top-[50px] left-[30px] w-[230px]">
+              <h5 className="font-semi-bold text-white text-heading pb-1 word-[-8px]">
+                {VALUES[3]?.title}
+              </h5>
+              <p className="text-white font-manrope font-regular text-body-sm">
+                {VALUES[3]?.content}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 export default Values
