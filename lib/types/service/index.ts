0fa9061a61cf033f88e1607f68efa4a3e7305362
imsumoregn <an12312123@gmail.com>
2022-07-13 13:58:01 +0100

    Update error response

--- lib/types/service/index.ts
+++ lib/types/service/index.ts
@@ -2,37 +2,37 @@ import { AxiosError, AxiosPromise, AxiosResponse } from 'axios'
 import { Mentee } from '../user'
 
 export interface Token {
-  accessToken: string
-  refreshToken: string
+	accessToken: string
+	refreshToken: string
 }
 
 export interface APIResponse<T = unknown>
-  extends AxiosResponse<{
-    isError: boolean
-    data: T
-    message: string
-  }> {}
+	extends AxiosResponse<{
+		isError: boolean
+		data: T
+		message: string
+	}> {}
 
 export interface RegisterParams {
-  email: string
-  password: string
-  name: string
+	email: string
+	password: string
+	name: string
 }
 export interface LogInParams {
-  email: string
-  password: string
+	email: string
+	password: string
 }
 
 export interface VerifyEmail {
-  (): Promise<{
-    hasVerifiedSuccess: boolean
-    message: string
-  }>
+	(): Promise<{
+		hasVerifiedSuccess: boolean
+		message: string
+	}>
 }
 
 export interface ChangePasswordParams {
-  password: string
-  passwordConfirmation: string
+	password: string
+	passwordConfirmation: string
 }
 
 /**
@@ -40,26 +40,24 @@ export interface ChangePasswordParams {
  */
 
 export interface ErrorResponse
-  extends Omit<AxiosError<APIResponse>, 'response'> {
-  response: {
-    data: APIResponse
-  }
+	extends Omit<AxiosError<APIResponse>, 'response'> {
+	response: APIResponse
 }
 
 const hasMessageError = (error: unknown): error is ErrorResponse => {
-  return (
-    typeof error === 'object' &&
-    error !== null &&
-    'message' in error &&
-    typeof (error as Record<string, unknown>).message === 'string'
-  )
+	return (
+		typeof error === 'object' &&
+		error !== null &&
+		'message' in error &&
+		typeof (error as Record<string, unknown>).message === 'string'
+	)
 }
 
 export const getErrorMessage = (error: unknown): string => {
-  if (hasMessageError(error)) {
-    const errorMessage = error.response.data.data.message
-    return errorMessage
-  }
+	if (hasMessageError(error)) {
+		const errorMessage = error?.response.data?.message
+		return errorMessage
+	}
 
-  return 'Unknown error.'
+	return 'Unknown error.'
 }
