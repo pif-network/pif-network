2620ab761b8ea4847adde1f3cc45095180c32cdb
Kimthanh11 <18047@student.vgu.edu.vn>
2022-07-16 12:11:33 +0700

    [Addition] Filter bar

--- components/ui/Select/index.tsx
+++ components/ui/Select/index.tsx
@@ -1,1 +1,74 @@
-export default {}
+import { ReactChildren } from 'react'
+import { ChevronRight } from '../svgs/Icons'
+
+interface GeneralSelectProps {
+	content: string
+	className?: string
+	options: Array<string>
+}
+
+const toggleSelectBox = () => {
+	const selectBox = document.querySelector('.options-container')
+	selectBox?.classList.toggle('hidden')
+}
+
+const selectOptions = (i: number) => {
+	const options = document.querySelectorAll(
+		'.option input[type="checkbox"]',
+	) as NodeListOf<HTMLInputElement>
+	const optionDivs = document.querySelectorAll('.option')
+	options.forEach((option, index) => {
+		if (index === i) {
+			if (!option.checked) {
+				option.checked = true
+				optionDivs[index]?.classList.add('bg-primary-100')
+			} else {
+				option.checked = false
+				optionDivs[index]?.classList.remove('bg-primary-100')
+			}
+		}
+	})
+}
+
+const Select = ({ content, className, options }: GeneralSelectProps) => {
+	return (
+		<div className="flex justify-center">
+			<div className="relative pl-8 select-box">
+				<div
+					onClick={toggleSelectBox}
+					className="py-2.5 px-7 border rounded-t-lg w-96 border-primary-900 bg-white text-gray-700 font-manrope font-bold text-sub-heading selected"
+				>
+					<span className="grid grid-cols-8 gap-4">
+						<div className="col-span-7">{content}</div>
+						<div>
+							<ChevronRight
+								colour="black"
+								className="transform rotate-90"
+							></ChevronRight>
+						</div>
+					</span>
+				</div>
+
+				<div className="absolute hidden overflow-hidden overflow-y-auto bg-white border rounded-b-lg border-b-black border-l-black border-r-black w-96 options-container max-h-48 shadow-gray-200">
+					{options.map((option, i) => (
+						<div
+							className="p-3 option hover:bg-primary-100 active:bg-primary-200"
+							onClick={() => selectOptions(i)}
+						>
+							<input
+								type="checkbox"
+								id={option}
+								className="w-4 h-4 checkbox accent-primary-400"
+							></input>
+							<label htmlFor={option} className="w-8 ml-3 text-body-lg">
+								{option}
+							</label>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Select
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,44 +5,45 @@ import { UserService } from '~/services'
 import { Mentor } from '~/lib/types/user'
 import { getErrorMessage } from '~/lib/types/service'
 import {
-  Skeleton,
-  Hero,
-  Benefits,
-  Values,
-  Feedback,
-  Button,
+	Skeleton,
+	Hero,
+	Benefits,
+	Values,
+	Feedback,
+	Button,
+	Select,
 } from '~/components/ui'
 import { MentorCard } from '~/components/mentor'
 import { FlagLine } from '~/components/ui/svgs/Icons'
 
 const HomePage = () => {
-  const [mentors, setMentors] = useState<Mentor[]>()
+	const [mentors, setMentors] = useState<Mentor[]>()
 
-  useEffect(() => {
-    const getAllMentors = async () => {
-      try {
-        const mentors = await UserService.getAllMentors()
-        setMentors(mentors)
-      } catch (error) {
-        const errorMessage = getErrorMessage(error)
-        console.log(errorMessage)
-      }
-    }
+	useEffect(() => {
+		const getAllMentors = async () => {
+			try {
+				const mentors = await UserService.getAllMentors()
+				setMentors(mentors)
+			} catch (error) {
+				const errorMessage = getErrorMessage(error)
+				console.log(errorMessage)
+			}
+		}
 
-    getAllMentors()
-  }, [])
+		getAllMentors()
+	}, [])
 
-  return (
-    <>
-      <Head>
-        <title>Homepage</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Homepage</title>
+			</Head>
 
-      <div>
-        <Hero />
-        <Benefits />
-        <Values />
-        {/* <div className="p-8 lg:p-24">
+			<div>
+				<Hero />
+				<Benefits />
+				<Values />
+				{/* <div className="p-8 lg:p-24">
           <div className="flex flex-wrap">
             {mentors ? (
               mentors.map(mentor => <MentorCard mentor={mentor} />)
@@ -51,11 +52,24 @@ const HomePage = () => {
             )}
           </div>
         </div> */}
+				<Select
+					content="Lĩnh vực"
+					options={['Product', 'HR', 'SWE', 'Data']}
+				></Select>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
+				<div>jafhjasfhjafhiasfhjakfsj</div>
 
-        <Feedback />
-      </div>
-    </>
-  )
+				<Feedback />
+			</div>
+		</>
+	)
 }
 
 export default HomePage
