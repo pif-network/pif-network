c1c7b7c2929b4429443053a068d2f4a0b18a22ff
Kimthanh11 <18047@student.vgu.edu.vn>
2022-08-11 21:59:06 +0700

    Update search page

---
+++ pages/search.tsx
@@ -0,0 +1,11 @@
+import FilterSection from '~/components/ui/FilterSection'
+
+const SearchPage = () => {
+	return (
+		<div>
+			<FilterSection></FilterSection>
+		</div>
+	)
+}
+
+export default SearchPage
