29998e6c8791aaa288c046add2941885ea90f79f
Tuong Vy <tuongvy27082002@gmail.com>
2022-08-11 18:07:58 +0700

    add logo shecodes

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -5,17 +5,17 @@ const NavBar = () => {
 	return (
 		<div className="container mx-auto sm:hidden md:block">
 			{/* <div className="flex "> */}
-			<nav className="flex items-center justify-between  pt-8 pb-8 px-20 ">
+			<nav className="flex items-center justify-between  pt-8 pb-8 px-16 ">
 				<div className="flex shrink-0  items-center font-semi-bold text-title-sm font-lora text-gray-700 mr-6 ">
-					{/* <Link href="/" prefetch={false}>
-					<img
-						src="/images/logo.png"
-						width={130}
-						height={33}
-						alt="SheCodes Mentor"
-					/>
-				</Link> */}
-					shecodes
+					<Link href="/" prefetch={false}>
+						<img
+							src="/images/logo.png"
+							width={130}
+							height={33}
+							alt="SheCodes Mentor"
+						/>
+					</Link>
+					{/* shecodes */}
 				</div>
 				<div className="inline-block ">
 					<div className="flex flex-wrap items-center">
