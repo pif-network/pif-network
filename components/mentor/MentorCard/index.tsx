392c31a9cabae17976e201ab08f986cf9af249e5
Tuong Vy <tuongvy27082002@gmail.com>
2022-08-14 09:17:59 +0700

    Make mentor card lazy loading

--- components/mentor/MentorCard/index.tsx
+++ components/mentor/MentorCard/index.tsx
@@ -2,47 +2,61 @@ import Link from 'next/link'
 
 import { Mentor } from '~/lib/types/user'
 import { FlagLine, PinLine } from '~/components/ui/svgs/Icons'
+import { Skeleton } from 'antd'
+import { useEffect, useState } from 'react'
 
 const MentorCard = ({ mentor }: { mentor: Partial<Mentor> }) => {
-  return (
-    <Link href={`mentors/${mentor.id}`}>
-      <div
-        className="card-mentor shadow-2xl hover:scale-105 rounded-xl overflow-hidden"
-        style={{ background: `url(${mentor.avatarUrl}) no-repeat center` }}
-      >
-        <div className="w-full h-full text-mask">
-          <div className="absolute bottom-0 left-[30px] ">
-            <h5 className="mb-0 text-white font-lora font-medium text-sub-heading">
-              {mentor.name}
-            </h5>
-            {mentor.exp && (
-              <p className="text-gray-200 font-manrope font-regular text-body-sm mb-3">
-                {mentor.exp[0]?.name}
-              </p>
-            )}
+	// const [isLoading, setIsLoading] = useState(true)
 
-            <ul className="mb-4 w-full">
-              {mentor.exp && (
-                <li className="flex">
-                  <PinLine colour="white" />
-                  <span className="text-white font-manrope font-regular text-body-md ml-2">
-                    {mentor.exp[0]?.position}
-                  </span>
-                </li>
-              )}
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		setIsLoading(false)
+	// 	}, 1000)
+	// }, [])
 
-              <li className="flex">
-                <FlagLine colour="white" width="16" height="16" />
-                <span className="text-white font-manrope font-regular text-body-md ml-2">
-                  {mentor.domainKnowledge}
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </Link>
-  )
+	return (
+		<Link href={`mentors/${mentor.id}`}>
+			{/* {isLoading ? (
+				<Skeleton.Avatar active size={200} shape="square" />
+			) : ( */}
+			<div
+				className="card-mentor shadow-2xl hover:scale-105 rounded-xl overflow-hidden animate__animated animate__fadeInRight"
+				style={{ background: `url(${mentor.avatarUrl}) no-repeat center` }}
+			>
+				<div className="w-full h-full text-mask">
+					<div className="absolute bottom-0 left-[30px] ">
+						<h5 className="mb-0 text-white font-lora font-medium text-sub-heading">
+							{mentor.name}
+						</h5>
+						{mentor.exp && (
+							<p className="text-gray-200 font-manrope font-regular text-body-sm mb-3">
+								{mentor.exp[0]?.name}
+							</p>
+						)}
+
+						<ul className="mb-4 w-full">
+							{mentor.exp && (
+								<li className="flex">
+									<PinLine colour="white" />
+									<span className="text-white font-manrope font-regular text-body-md ml-2">
+										{mentor.exp[0]?.position}
+									</span>
+								</li>
+							)}
+
+							<li className="flex">
+								<FlagLine colour="white" width="16" height="16" />
+								<span className="text-white font-manrope font-regular text-body-md ml-2">
+									{mentor.domainKnowledge}
+								</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			{/* )} */}
+		</Link>
+	)
 }
 
 export default MentorCard
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,14 +1,42 @@
+import { Card, Skeleton } from 'antd'
+import { useEffect, useState } from 'react'
 import { MentorCard } from '~/components/mentor'
 import { SectionTitle } from '~/components/ui'
 import { RANDOM_MENTORS } from '~/shared/constant'
 
 const SearchPage = () => {
-	const mentors = [...RANDOM_MENTORS, ...RANDOM_MENTORS, ...RANDOM_MENTORS]
+	const [mentors, setMentor] = useState([
+		...RANDOM_MENTORS,
+		...RANDOM_MENTORS,
+		...RANDOM_MENTORS,
+	])
+
+	const observer = new IntersectionObserver(
+		entries => {
+			const lastCard = entries[0]!
+			if (lastCard.isIntersecting) {
+				setMentor([...mentors, ...RANDOM_MENTORS])
+				observer.unobserve(lastCard.target)
+			}
+		},
+		{
+			threshold: 1,
+		},
+	)
+
+	useEffect(() => {
+		if (mentors.length < 24) {
+			const container = document.getElementById('container')
+			const cards = container!.children
+			const lastCard = cards[cards.length - 1]
+			observer.observe(lastCard!)
+		}
+	}, [mentors])
 
 	return (
 		<div className="container mx-auto">
 			<SectionTitle content="Lựa chọn mentor phù hợp với bạn tại đây" />
-			<div className="flex flex-row flex-wrap gap-y-4 py-6">
+			<div id="container" className="flex flex-row flex-wrap gap-y-4 py-6">
 				{mentors.map((mentor, idx) => (
 					<div key={idx} className="basis-1/2 lg:basis-1/4 px-2">
 						<div className="flex justify-center items-center">
