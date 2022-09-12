082122147bab06f7a6878cf193cb7c5131d8501c
Kimthanh11 <18047@student.vgu.edu.vn>
2022-09-12 16:26:55 +0700

    [Update] Make FeedbackCard dynamic

--- components/ui/Feedback/feedback_card.tsx
+++ components/ui/Feedback/feedback_card.tsx
@@ -2,15 +2,20 @@ import { ChevronRight } from '../svgs/Icons'
 import DoubleQuoteSm from '~/assets/feedback/feedback_double-quote-sm.png'
 import DoubleQuoteLg from '~/assets/feedback/feedback_double-quote-lg.png'
 import LineLG from '~/assets/feedback/feedback_line-lg.png'
-import MenteeAvatar from '~/assets/feedback/feedback_avatar.png'
+
 
 interface feedbackCard {
   isActive: boolean
-  onShow: () => void
-
+  onPrevious: () => void
+  onNext: () => void
+  mainFeedback: string
+  subFeedback: string
+  name: string
+  job: string
+  avatar: string
 }
 
-const FeedbackCard = ({ isActive, onShow }: feedbackCard) => {
+const FeedbackCard = ({ isActive, onPrevious, onNext,  mainFeedback, subFeedback, name, job, avatar }: feedbackCard) => {
   return (
     <>
       {isActive && (
@@ -28,12 +33,10 @@ const FeedbackCard = ({ isActive, onShow }: feedbackCard) => {
 
               <div className="grid items-center justify-center absolute md:top-[83px] md:left-[121px] md:w-[830px] sm:top-[24px] sm:left-[36px]">
                 <p className="text-black font-lora font-semi-bold md:text-[32px] md:leading-10 md:word-[-8px] sm:word-[-3px] sm:text-[10px] text-left mb-16 md:mb-0">
-                  My gave me a lot of valuable advice, especially how to improve
-                  my portfolio, as well as how to correctly communicate with
-                  people.
+                  {mainFeedback}
                 </p>
                 <p className="text-gray-400 font-manrope font-regular text-semi-bold md:text-[24px] sm:text-[8px] md:word-[3px] sm:word-[-0.3px] text-left absolute md:static">
-                  And something more that I don’t remember
+                  {subFeedback}
                 </p>
               </div>
             </div>
@@ -46,21 +49,21 @@ const FeedbackCard = ({ isActive, onShow }: feedbackCard) => {
             <div className="flex flex-row justify-between">
               <div>
                 <img
-                  src={MenteeAvatar.src}
+                  src={`${avatar}`}
                   className="absolute hidden md:inline-block md:top-[306px] md:left-[121px] md:w-[64px] md:h-[64px]"
                 ></img>
 
                 <img
-                  src={MenteeAvatar.src}
-                  className=" absolute md:hidden sm:top-[91px] sm:left-[34px] sm:w-[19px] sm:h-[19px]"
+                  src={`${avatar}`}
+                  className="absolute md:hidden sm:top-[91px] sm:left-[34px] sm:w-[19px] sm:h-[19px]"
                 ></img>
 
                 <div className="absolute md:top-[316px] md:left-[195px] sm:top-[92px] sm:left-[55px]">
                   <p className="text-primary-900 font-lora font-semi-bold md:text-sub-heading sm:text-[7px] md:word-[-6px] sm:word-[-2px]">
-                    Nguyễn Mai Anh
+                    {name}
                   </p>
                   <p className="text-gray-600 font-manrope md:text-caption sm:text-[4px] md:word-[-0.8px] sm:word-[-0.3px]">
-                    Student, UEH.
+                   {job}
                   </p>
                 </div>
               </div>
@@ -69,23 +72,23 @@ const FeedbackCard = ({ isActive, onShow }: feedbackCard) => {
                 <ChevronRight
                   colour="black"
                   className="transform rotate-180 absolute hidden md:inline-block md:left-[840px] md:top-[333px]"
-                  onClick={onShow}
+                  onClick={onNext}
                 ></ChevronRight>
                 <ChevronRight
                   colour="black"
                   className="absolute hidden md:inline-block md:left-[884px] md:top-[333px]"
-                  onClick={onShow}
+                  onClick={onNext}
                 ></ChevronRight>
 
                 <ChevronRight
                   colour="black"
                   className="transform rotate-180 absolute sm:w-[6px] md:hidden sm:left-[247px] sm:top-[90px]"
-                  onClick={onShow}
+                  onClick={onPrevious}
                 ></ChevronRight>
                 <ChevronRight
                   colour="black"
                   className="absolute md:hidden sm:w-[6px] sm:left-[260px] sm:top-[90px]"
-                  onClick={onShow}
+                  onClick={onPrevious}
                 ></ChevronRight>
               </div>
             </div>
--- components/ui/Feedback/index.tsx
+++ components/ui/Feedback/index.tsx
@@ -1,5 +1,7 @@
 import FeedbackCard from './feedback_card'
 import { useState } from 'react'
+import MenteeAvatar from '~/assets/feedback/feedback_avatar.png'
+
 
 const Feedback = () => {
   const [activeIndex, setActiveIndex] = useState(0)
@@ -8,15 +10,31 @@ const Feedback = () => {
       <div className="grid text-black font-lora font-semi-bold md:text-title-sm sm:text-heading-sm md:pb-11 sm:pb-8 justify-items-center sm:word-[-7px] md:word-[-10px]">
         Mentee nói gì về &lt;product_name&gt;?
       </div>
+
       <FeedbackCard
         isActive={activeIndex === 0}
-        onShow={() => {
-          setActiveIndex(1)
-        }}
+        onNext={() => setActiveIndex(1)}
+        onPrevious={() => setActiveIndex(1)}
+        mainFeedback="My gave me a lot of valuable advice, especially how to improve
+        my portfolio, as well as how to correctly communicate with
+        people."
+        subFeedback='And something more that I don’t remember'
+        name='Nguyễn Mai Anh'
+        job='Student, UEH.'
+        avatar={`${MenteeAvatar.src}`}
       ></FeedbackCard>
+
       <FeedbackCard
         isActive={activeIndex === 1}
-        onShow={() => setActiveIndex(0)}
+        onNext={() => setActiveIndex(0)}
+        onPrevious={() => setActiveIndex(0)}
+        mainFeedback="My gave me a lot of valuable advice, especially how to improve
+        my portfolio, as well as how to correctly communicate with
+        people."
+        subFeedback='And something more that I don’t remember'
+        name='Nguyễn Mai Anh'
+        job='Student, UEH.'
+        avatar={`${MenteeAvatar.src}`}
       ></FeedbackCard>
     </div>
   )
