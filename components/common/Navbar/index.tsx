463a642814d83298ca7413d7f21db107b91b5f23
imsumoregn <an12312123@gmail.com>
2022-09-21 20:22:38 +0100

    Re-position Navbar

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -2,29 +2,24 @@ import Link from 'next/link'
 import UserNav from './UserNav'
 
 const NavBar = () => {
-	return (
-		<div className="container mx-auto sm:hidden md:block">
-			{/* <div className="flex "> */}
-			<nav className="flex items-center justify-between  pt-8 pb-8 px-16 ">
-				<div className="flex shrink-0  items-center font-semi-bold text-title-sm font-lora text-gray-700 mr-6 ">
-					<Link href="/" prefetch={false}>
-						<img
-							src="/images/logo.png"
-							width={130}
-							height={33}
-							alt="SheCodes Mentor"
-						/>
-					</Link>
-					{/* shecodes */}
-				</div>
-				<div className="inline-block ">
-					<div className="flex flex-wrap items-center">
-						<UserNav />
-					</div>
-				</div>
-			</nav>
-		</div>
-	)
+  return (
+    <nav className="fixed w-full sm:hidden md:block">
+      <div className="flex container items-center justify-between gap-0 mx-auto py-8">
+        <div className="flex items-center">
+          <Link href="/" prefetch={false}>
+            <img
+              src="/images/logo.png"
+              width={130}
+              height={33}
+              alt="SheCodes Mentor"
+            />
+          </Link>
+        </div>
+
+        <UserNav />
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
