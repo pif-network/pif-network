06c30cc826057586835836d4b0c791eed9d4d826
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-10-03 16:22:11 +0700

    fix: consistent image size achieved

--- components/ui/brand-explained.tsx
+++ components/ui/brand-explained.tsx
@@ -8,39 +8,50 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 import talk from '~/public/talk.png';
 
 const BrandExplained = () => {
-  const ref = useRef(null);
+  const ref = useRef<HTMLElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['start end', 'end end'],
   });
   const imgTransformX = useTransform(scrollYProgress, [0.3, 1], [0, -300]);
-  const txtTransformX = useTransform(scrollYProgress, [0.3, 1], [100, 0]);
+  const txtTransformY = useTransform(scrollYProgress, [0.3, 1], [100, 0]);
+  const txtTransformX = useTransform(scrollYProgress, [0.3, 1], [0, 80]);
+
+  if (ref.current) {
+    console.log(ref.current.getBoundingClientRect());
+  }
 
   return (
-    <section ref={ref} className="">
-      <motion.div className="mx-3 flex gap-4" style={{ x: imgTransformX }}>
+    <section ref={ref} className="flex gap-4">
+      {/* <motion.div className="mx-3" style={{ x: imgTransformX }}> */}
+      <motion.div className="mx-3">
         <Image
+          className="max-w-sm"
           src={talk}
           alt="Talking at PIF."
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          sizes="(max-width: 500px) 100vw, 50vw"
           style={{
-            width: '100%',
+            // width: '100%',
             height: 'auto',
           }}
           width={300}
           height={400}
         />
-
-        <motion.div className="mr-4" style={{ y: txtTransformX }}>
-          <h2 className="font-lora font-bold text-center text-[18px] w-[300px]">
-            Giá trị của sự <span className="inline-block">trò chuyện.</span>
-          </h2>
-          <p>
-            Không phải ai sinh ra bẩm sinh là một mentor, cũng không phải sinh
-            ra đều là mentee hợp cách.
-          </p>
-        </motion.div>
       </motion.div>
+
+      <motion.section
+        className="mr-4"
+        // style={{ x: txtTransformX, y: txtTransformY }}
+      >
+        {/* <motion.section className="mr-4"> */}
+        <h2 className="font-lora font-bold text-center text-[18px] w-[300px]">
+          Giá trị của sự <span className="inline-block">trò chuyện.</span>
+        </h2>
+        <p>
+          Không phải ai sinh ra bẩm sinh là một mentor, cũng không phải sinh ra
+          đều là mentee hợp cách.
+        </p>
+      </motion.section>
     </section>
   );
 };
