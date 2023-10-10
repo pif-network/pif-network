80ca7d67db949a9fe6de8af0ff1bd491b21e3f85
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-10-10 18:27:43 +0700

    feat(ui): setup tooltip

--- components/ui/role-choosing-popover.tsx
+++ components/ui/role-choosing-popover.tsx
@@ -2,6 +2,11 @@ import { UserRole } from '~/lib/types/user';
 import { USER_ROLE } from '~/shared/constant';
 
 import { Popover } from 'antd';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '~/components/ui/tooltip';
 
 interface RoleChoosingPopoverProps {
   userType: UserRole;
@@ -9,17 +14,10 @@ interface RoleChoosingPopoverProps {
   disabled: boolean | undefined;
 }
 
-const MentorPopoverContent = () => (
-  <div>
-    Trở thành mentor ngay để đồng hành cùng các bạn trẻ yêu mến lập trình trên
-    khắp cả nước.
-  </div>
-);
+const MentorPopoverContent = () => <div>Pay it forward.</div>;
 const MenteePopoverContent = () => (
   <div>
-    Trở thành mentee ngay để nhận được sự hỗ trợ, đồng hành từ các mentors của{' '}
-    <br />
-    SheCodes trên con đường dấn thân vào ngành công nghệ của bạn.
+    <i>Will</i> pay it forward.
   </div>
 );
 
@@ -30,9 +28,10 @@ const RoleChoosingPopoverContent = {
 
 export default ({ userType, onClick, disabled }: RoleChoosingPopoverProps) => {
   return (
-    <Popover placement="bottom" content={RoleChoosingPopoverContent[userType]}>
-      <button
-        className={`px-6 py-3 transition ease-in-out duration-600 box-border border-[1px] border-black/75 hover:border-primary-100
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <button
+          className={`px-6 py-3 transition ease-in-out duration-600 box-border border-[1px] border-black/75 hover:border-primary-100
           hover:bg-primary-100 rounded-sm hover:text-white font-lora font-semi-bold text-[18px]
           ${
             disabled === undefined
@@ -41,11 +40,13 @@ export default ({ userType, onClick, disabled }: RoleChoosingPopoverProps) => {
               ? 'border-black/50 text-black/60 scale-95'
               : 'bg-primary-300 text-white border-primary-300 scale-105 shadow-2xl'
           }`}
-        type="button"
-        onClick={onClick}
-      >
-        {userType}
-      </button>
-    </Popover>
+          type="button"
+          onClick={onClick}
+        >
+          {userType}
+        </button>
+      </TooltipTrigger>
+      <TooltipContent>{RoleChoosingPopoverContent[userType]()}</TooltipContent>
+    </Tooltip>
   );
 };
