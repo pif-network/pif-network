6e79040cb1553bae333b6f7d4fd007b642a88296
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-12-08 03:20:54 +0700

    chore(db): seed offers + fields

--- server/db/seed.ts
+++ server/db/seed.ts
@@ -1,19 +1,40 @@
 /* This file is runned by bun. */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { PrismaClient } from '@prisma/client';
+import { FIELD_METADATA, OFFER_METADATA } from '~/shared/constant';
 
 const prisma = new PrismaClient();
+
+const offerSeed = Object.keys(OFFER_METADATA).map(key => {
+  return {
+    name: key,
+    description: 'PLACEHOLDER',
+  };
+});
+
+const fieldSeed = Object.keys(FIELD_METADATA).map(key => {
+  return {
+    name: key,
+    description: 'PLACEHOLDER',
+  };
+});
 
 try {
   console.log('Seeding database...');
   /* @ts-ignore-next-line */
-  const r = await prisma.offer.create({
-    data: {
-      name: 'CAREER_ORIENTATION',
-      description: 'Helping you find your way in the world of work',
-    },
+  // drop offer + field table
+  await prisma.offer.deleteMany({});
+  /* @ts-ignore-next-line */
+  await prisma.field.deleteMany({});
+
+  /* @ts-ignore-next-line */
+  await prisma.offer.createMany({
+    data: offerSeed,
   });
-  console.log(r);
+  /* @ts-ignore-next-line */
+  await prisma.field.createMany({
+    data: fieldSeed,
+  });
 } catch (e) {
   console.error(e);
 } finally {
