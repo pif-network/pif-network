74b9702616a7ff212074df88bef0975987b19c81
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-12-13 23:54:22 +0700

    feat(db): truncate before seeding

--- server/db/seed.ts
+++ server/db/seed.ts
@@ -5,13 +5,22 @@ import { FIELD_METADATA, OFFER_METADATA } from '~/shared/constant';
 
 const prisma = new PrismaClient();
 
+/* @ts-ignore-next-line */
+const tablenames = await prisma.$queryRaw<
+  Array<{ tablename: string }>
+>`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+const tables = tablenames
+  .map(({ tablename }) => tablename)
+  .filter(name => name !== '_prisma_migrations')
+  .map(name => `"public"."${name}"`)
+  .join(', ');
+
 const offerSeed = Object.keys(OFFER_METADATA).map(key => {
   return {
     name: key,
     description: 'PLACEHOLDER',
   };
 });
-
 const fieldSeed = Object.keys(FIELD_METADATA).map(key => {
   return {
     name: key,
@@ -20,12 +29,10 @@ const fieldSeed = Object.keys(FIELD_METADATA).map(key => {
 });
 
 try {
+  /* @ts-ignore-next-line */
+  await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+
   console.log('Seeding database...');
-  /* @ts-ignore-next-line */
-  // drop offer + field table
-  await prisma.offer.deleteMany({});
-  /* @ts-ignore-next-line */
-  await prisma.field.deleteMany({});
 
   /* @ts-ignore-next-line */
   await prisma.offer.createMany({
