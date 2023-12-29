8caa7494a531e7f8e45a9910b4c2c63314c5480e
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-12-29 17:35:28 +0700

    ui: social link comp

--- app/(main)/mentors/[user-name]/page.tsx
+++ app/(main)/mentors/[user-name]/page.tsx
@@ -5,10 +5,29 @@ import {
   Avatar,
   AvatarFallback,
   AvatarImage,
+  Link,
 } from '~/components/ui';
-import { LocationFill, MortarboardHatFill } from '~/components/ui/svgs/icons';
+import {
+  GithubFill,
+  LocationFill,
+  MortarboardHatFill,
+} from '~/components/ui/svgs/icons';
 
 import { User } from '@prisma/client';
+
+const SquareSocialLink = ({
+  href,
+  icon,
+}: {
+  href: string;
+  icon: React.ReactNode;
+}) => (
+  <div className="flex items-center justify-center w-10 h-10 bg-white rounded-xl shadow-[0px_4px_48px_rgba(0,0,0,0.08),0px_8px_24px_rgba(0,0,0,0.12)] hover:-translate-y-[2px] duration-200 ease-out cursor-pointer">
+    <Link external href={href} className="text-sub-heading">
+      {icon}
+    </Link>
+  </div>
+);
 
 const MentorProfilePage = () => {
   const data: Partial<User> = {
@@ -18,13 +37,15 @@ const MentorProfilePage = () => {
     schoolName: 'University of California, Berkeley',
     major: 'Computer Science',
     location: 'San Francisco, CA',
+    githubUrl: '#',
+    linkedinUrl: '#',
   };
   const url = 'https://github.com/shadcn.png';
   return (
     <main className="h-screen pt-28">
       <section>
         <div className="mx-24 flex">
-          <div className="w-40">
+          <div className="w-44 mr-14">
             <AspectRatio ratio={2 / 3}>
               <Avatar className="inline">
                 <AvatarImage
@@ -37,18 +58,34 @@ const MentorProfilePage = () => {
           </div>
 
           <div className="flex flex-col">
-            <h1 className="max-w-[12ch] text-title-sm lg:text-[36px] text-primary-900 font-lora font-bold mb-3">
+            <div className="mb-7" />
+
+            <h2 className="max-w-[12ch] text-title-sm lg:text-[36px] text-primary-900 font-lora font-bold">
               {data.name}
-            </h1>
-            <div className="text-body">
+            </h2>
+
+            <div className="mb-3" />
+
+            <div>
+              <SquareSocialLink
+                href={data.githubUrl!}
+                icon={<GithubFill colour="black" />}
+              />
+            </div>
+
+            <div className="mb-3" />
+
+            <div className="text-body-sm">
               <MortarboardHatFill
                 className="inline-block mr-3"
                 colour="black"
               />
               {`Tốt nghiệp chuyên ngành ${data.major} tại ${data.schoolName}`}
             </div>
+
             <div className="mb-2" />
-            <div className="text-body">
+
+            <div className="text-body-sm">
               <LocationFill className="inline-block mr-3" colour="black" />
               {data.location}
             </div>
