f991dc15574e8f6d26e31401d629cf78c66b50a8
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2024-01-04 17:01:22 +0700

    ui: modify tag colours

--- components/ui/tag.tsx
+++ components/ui/tag.tsx
@@ -15,35 +15,6 @@ interface TagProps extends HTMLAttributes<HTMLElement> {
   onDelete?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
-const hoverPresetByType = {
-  purple: {
-    outlined: 'hover:text-purple-400 ',
-    filled: 'hover:border-purple-400 ',
-  },
-  red: {
-    outlined: 'hover:text-red-300 hover:border-red-300',
-    filled: 'hover:border-red-300 hover:bg-red-200',
-  },
-  cyan: {
-    outlined: 'hover:text-yan-300 hover:border-cyan-300',
-    filled: 'hover:border-can-300 hover:bg-cyan-200',
-  },
-  prussian: {
-    outlined: 'hover:text-yan-300 hover:border-cyan-300',
-    filled: 'hover:border-can-300 hover:bg-cyan-200',
-  },
-};
-
-const deleteBgByType = Object.values(FIELD_METADATA).reduce((acc, value) => {
-  acc[value.tagColour] = `bg-${value.tagColour}-200`;
-  return acc;
-}, {} as { [Property in TagColour]: string });
-
-const defaultPresetByType = {
-  outlined: 'text-gray-600 bg-white border-gray-400',
-  filled: 'text-gray-50 bg-gray-400 border-gray-700',
-};
-
 const tagVariants = cva(
   'cursor-pointer border font-semi-bold font-manrope text-caption rounded-3xl px-2 py-1 inline-block relative group transition-colors ease-out duration-300',
   {
@@ -53,47 +24,47 @@ const tagVariants = cva(
         outline: 'text-gray-600 border-gray-400',
       },
       colour: {
-        purple: 'hover:border-purple-400',
-        cyan: 'hover:border-cyan-300',
-        red: 'hover:border-red-400',
-        prussian: 'hover:border-prussian-400',
+        purple: '',
+        cyan: '',
+        red: '',
+        prussian: 'hover:border-prussian-200',
       },
     },
     compoundVariants: [
       {
         type: 'fill',
         colour: 'purple',
-        className: 'hover:bg-purple-300',
+        className: 'hover:border-primary-800 hover:bg-primary-300',
       },
       {
         type: 'outline',
         colour: 'purple',
-        className: 'hover:text-purple-400 ',
+        className: 'hover:border-primary-300 hover:text-primary-700 ',
       },
       {
         type: 'fill',
         colour: 'cyan',
-        className: 'hover:bg-cyan-300 ',
+        className: 'hover:border-cyan-400 hover:bg-cyan-300 ',
       },
       {
         type: 'outline',
         colour: 'cyan',
-        className: 'hover:text-cyan-300 ',
+        className: 'hover:border-cyan-100 hover:text-cyan-300 ',
       },
       {
         type: 'fill',
         colour: 'red',
-        className: 'hover:bg-red-300 ',
+        className: 'hover:border-red-400 hover:bg-red-300 ',
       },
       {
         type: 'outline',
         colour: 'red',
-        className: 'hover:text-red-300 ',
+        className: 'hover:border-red-100 hover:text-red-300 ',
       },
       {
         type: 'fill',
         colour: 'prussian',
-        className: 'hover:bg-prussian-300 ',
+        className: 'hover:bg-prussian-400 ',
       },
       {
         type: 'outline',
