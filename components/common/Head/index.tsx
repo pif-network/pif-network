077f936150782d2fe5647a36773849b362c57409
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2024-01-04 17:42:19 +0700

    fix: revert miss-rename

--- components/common/Head/index.tsx
+++ components/common/Head/index.tsx
@@ -21,7 +21,7 @@ const Head = () => {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" colour="#5bbad5" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
 
       <meta name="description" content="SheCodes Vietnam Mentorship" />
