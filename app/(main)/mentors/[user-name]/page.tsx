97003d9d2325420f431f076e1257c6b650342902
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2024-01-26 05:18:13 +0700

    ui: place btn + small res

--- app/(main)/mentors/[user-name]/page.tsx
+++ app/(main)/mentors/[user-name]/page.tsx
@@ -19,6 +19,7 @@ import { FIELD_METADATA, OFFER_METADATA } from '~/shared/constant';
 import {
   CounterClockwiseClockIcon,
   LinkedInLogoIcon,
+  Pencil2Icon,
 } from '@radix-ui/react-icons';
 
 const SquareSocialLink = ({
@@ -206,14 +207,21 @@ const MentorProfilePage = () => {
         </div>
       </section>
 
-      <div className="mb-4 lg:mb-0" />
+      <div className="mb-6 lg:mb-0" />
 
-      <section>
-        <SectionTitle size="small">
-          <h2 className="font-lora font-bold text-heading-md">
-            Đánh giá từ mentee
-          </h2>
-        </SectionTitle>
+      <section className="max-w-[525px]">
+        <div className="flex justify-between">
+          <SectionTitle size="small">
+            <h2 className="font-lora font-bold text-heading-md">
+              Đánh giá từ mentee
+            </h2>
+          </SectionTitle>
+
+          <Button variant="outline" className="h-8 lg:h-12 px-3 lg:px-7">
+            <Pencil2Icon className="w-3 h-3 lg:w-5 lg:h-5 lg:mr-2" />
+            <span className="hidden lg:inline"> Viết đánh giá</span>
+          </Button>
+        </div>
 
         <div className="mb-4" />
 
